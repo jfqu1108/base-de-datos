5fc019cf64f6a2bc69cc9069451c687533145d5f
jfqu1108 <jfqu1108@gmail.com>
2023-12-05 15:02:57 -0500

    hacinedo conexiones con js

---
+++ app.js
@@ -0,0 +1,4 @@
+const inputNombre = document.querySelector("#nombre");
+const inputEmail = document.querySelector("#email");
+const inputContraseña = document.querySelector("#contraseña");
+const inputConfirmContraseña = document.querySelector("#Confirmar_contraseña");
