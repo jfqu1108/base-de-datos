869e2d443142129dd051947ca112b3b6410f7738
jfqu1108 <jfqu1108@gmail.com>
2023-12-05 15:09:59 -0500

    agregando alertas al fomulario

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const inputNombre = document.querySelector("#nombre");
 const inputEmail = document.querySelector("#email");
 const inputContraseña = document.querySelector("#contraseña");
 const inputConfirmContraseña = document.querySelector("#Confirmar_contraseña");
+
+const formulario = document.querySelector(".formulario");
+
+const alert = document.createElement("DIV") //crea el elemento de la alerta
